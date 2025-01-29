3c29f0663d01a1ddfa2dedb9d72ad81e47a045a4
Đỗ Thành <126317114+thanhdo1110@users.noreply.github.com>
2025-01-29 14:56:25 +0700

    Update check_key_replace.js

--- check_key_replace.js
+++ check_key_replace.js
@@ -1,9 +1,7 @@
-const url = "https://serverkey.hhnios.site/";
-
-$httpClient.get(url, function (error, response, data) {
-    if (error) {
-        $done({ body: JSON.stringify({ status: "error", message: "Request failed" }) });
-    } else {
-        $done({ body: data });
-    }
+// check_key_replace.js - Quantumult X Script
+var newBody = JSON.stringify({
+    "key": "HdangIos-fe6b37d44e4240fba018d6461a09152e",
+    "uuid": "9AA5670A-7ED5-4173-9932-57FFA38309AD"
 });
+
+$done({ body: newBody });
