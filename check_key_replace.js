7beaa32ff9ea1f50a4d19b9be1bb363e95970b06
Đỗ Thành <126317114+thanhdo1110@users.noreply.github.com>
2025-01-29 15:08:19 +0700

    Update check_key_replace.js

--- check_key_replace.js
+++ check_key_replace.js
@@ -1,7 +1,7 @@
 // check_key_replace.js - Quantumult X Script
 var newBody = JSON.stringify({
     "activation_date": "29-01-2025",
-    "amount": "30-01-2025",
+    "amount": "99-99-9999",
     "message": "Key hợp lệ.",
     "status": "success"
 });
