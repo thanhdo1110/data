d2717a0aa0ed6d1e5014ee13746fa719fc01e3b5
Đỗ Thành <126317114+thanhdo1110@users.noreply.github.com>
2025-04-14 18:06:15 +0700

    Update check_key_replace.js

--- check_key_replace.js
+++ check_key_replace.js
@@ -1,6 +1,4 @@
 // check_key_replace.js - Quantumult X Script
-var newBody = JSON.stringify({
-PMuPmATmEj1Q+Ve902Cqjv/Rul77v5DJvyaydPTJinx9yZbCmTgmLsWf479fHqDnvAoK1QpRLWEy7Xh8xzIUK5E692olguu2wmERcpnlyKgu02dJ//pfBs/099ZKy+KMvMsjyiZiLmEzq5SyGAfvIdp0RdrrCik2QG+Vg3iFY0fwz+UR7ZfRDrhJJJDv14WaatxYWceFn1G0dpgkZH7VDObq5CiiIImpjkPZwM+lejQHqz0GhC90NcGUuSWGR1JE
-});
+var newBody = "PMuPmATmEj1Q+Ve902Cqjv/Rul77v5DJvyaydPTJinx9yZbCmTgmLsWf479fHqDnvAoK1QpRLWEy7Xh8xzIUK5E692olguu2wmERcpnlyKgu02dJ//pfBs/099ZKy+KMvMsjyiZiLmEzq5SyGAfvIdp0RdrrCik2QG+Vg3iFY0fwz+UR7ZfRDrhJJJDv14WaatxYWceFn1G0dpgkZH7VDObq5CiiIImpjkPZwM+lejQHqz0GhC90NcGUuSWGR1JE";
 
 $done({ body: newBody });
