c88ee4013f40ee11f08e761920af57afadd144a1
Đỗ Thành <126317114+thanhdo1110@users.noreply.github.com>
2025-04-15 23:37:53 +0700

    Update check_key_replace.js

--- check_key_replace.js
+++ check_key_replace.js
@@ -1,7 +1,7 @@
 // check_key_replace.js - Quantumult X Script
 
 // Trả về đúng chuỗi bạn muốn
-var newBody = "PMuPmATmEj1Q+Ve902CqjtoRW1dwB1lFHrdBpykvk/GdsgAumF3j1NYCvpRxmj1mEO+FIrXVr5dDLEA4uY0cISMfOw0Hbd1moWsS51GCPEku4I16FVY/FwHEVN7I9lFwh9A4Tl7M9nDrIpk2Mk9V4IwhNLNCTGKOw26KeG76vRV3fPziPq5mu6mqlgYRa3EmNfWKCPIkSBpBuwzqTWKWlqEFmDSr7z1LKRcp6al6SFFOTNHfwHoRMqH8KPkJOWORwkpfcKZFGFY0QnGInGQaQDGNYJd";
+var newBody = "PMuPmATmEj1Q+Ve902CqjtoRW1dwB1lFHrdBpykvk/GdsgAumF3j1NYCvpRxmj1mEO+FIrXVr5dDLEA4uY0cISMfOw0Hbd1moWsS51GCPEku4I16FVY/FwHEVN7I9lFwh9A4Tl7M9nDrIpk2Mk9V4IwhNLNCTGKOw26KeG76vRV3fPziPq5mu6mqlgYRa3EmNfWKCPIkSBpBuwzqTWKWlqEFmDSr7z1LKRcp6al6SFFOTNHfwHoRMqH8KPkJOWORwkpfcKZFGFY0QnGInGQaQDGNYJd+kmdgYXQRkwRQVUKitgllRpfKv5FwYrJj7bEF1dh7JfDIJDpcUXK1Hi8fV9HhWQITOLSMH9tMOf+erBJ5HMjzGWFDgrlPY/xMuJhy";
 
 // Trả về body chỉ chứa chuỗi đó mà không phải là một đối tượng JSON
 $done({ body: newBody });
